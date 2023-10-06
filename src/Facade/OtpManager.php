a17f80fbcaf4d1ed121e3cd15639b6dbd0c148a2
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-10-06 16:46:15 +0330

    add phpdocs to OtpManager facade

--- src/Facade/OtpManager.php
+++ src/Facade/OtpManager.php
@@ -2,8 +2,23 @@
 
 namespace Salehhashemi\OtpManager\Facade;
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Facade;
+use Salehhashemi\OtpManager\Contracts\OtpTypeInterface;
+use Salehhashemi\OtpManager\Dto\OtpDto;
+use Salehhashemi\OtpManager\Dto\SentOtpDto;
 
+/**
+ * @method static SentOtpDto send(string $mobile, OtpTypeInterface $type = null)
+ * @method static SentOtpDto sendAndRetryCheck(string $mobile, OtpTypeInterface $type = null)
+ * @method static bool verify(string $mobile, int $otp, string $trackingCode, OtpTypeInterface $type = null)
+ * @method static OtpDto|null getVerifyCode(string $mobile, OtpTypeInterface $type = null)
+ * @method static bool deleteVerifyCode(string $mobile, OtpTypeInterface $type = null)
+ * @method static Carbon|null getSentAt(string $mobile, OtpTypeInterface $type = null)
+ * @method static bool isVerifyCodeHasBeenSent(string $mobile, OtpTypeInterface $type = null)
+ *
+ * @see \Salehhashemi\OtpManager\OtpManager
+ */
 class OtpManager extends Facade
 {
     /**
