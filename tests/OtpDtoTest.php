e28ca3162ccf6ed4cbfaa7ac9538846801bf64a7
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-10-07 23:44:52 +0330

    add tests for dtos

---
+++ tests/OtpDtoTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Salehhashemi\OtpManager\Tests;
+
+use Salehhashemi\OtpManager\Dto\OtpDto;
+
+class OtpDtoTest extends BaseTest
+{
+    public function test_constructor_initializes_properties_correctly(): void
+    {
+        $code = 123456;
+        $trackingCode = '550e8400-e29b-41d4-a716-446655440000';
+
+        $otpDto = new OtpDto($code, $trackingCode);
+
+        $this->assertSame($code, $otpDto->code);
+        $this->assertSame($trackingCode, $otpDto->trackingCode);
+    }
+
+    public function test_toArray_returns_correct_array(): void
+    {
+        $code = 123456;
+        $trackingCode = '550e8400-e29b-41d4-a716-446655440000';
+
+        $otpDto = new OtpDto($code, $trackingCode);
+
+        $expectedArray = [
+            'code' => $code,
+            'tracking_code' => $trackingCode,
+        ];
+
+        $this->assertSame($expectedArray, $otpDto->toArray());
+    }
+}
---
+++ tests/SentOtpDtoTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Salehhashemi\OtpManager\Tests;
+
+use Salehhashemi\OtpManager\Dto\SentOtpDto;
+
+class SentOtpDtoTest extends BaseTest
+{
+    public function test_constructor_initializes_properties_correctly(): void
+    {
+        $code = 123456;
+        $waitingTime = 60;
+        $trackingCode = '550e8400-e29b-41d4-a716-446655440000';
+
+        $sentOtpDto = new SentOtpDto($code, $waitingTime, $trackingCode);
+
+        $reflectionClass = new \ReflectionClass(SentOtpDto::class);
+
+        $codeProperty = $reflectionClass->getProperty('code');
+        $codeProperty->setAccessible(true);
+
+        $waitingTimeProperty = $reflectionClass->getProperty('waitingTime');
+        $waitingTimeProperty->setAccessible(true);
+
+        $trackingCodeProperty = $reflectionClass->getProperty('trackingCode');
+        $trackingCodeProperty->setAccessible(true);
+
+        $this->assertSame($code, $codeProperty->getValue($sentOtpDto));
+        $this->assertSame($waitingTime, $waitingTimeProperty->getValue($sentOtpDto));
+        $this->assertSame($trackingCode, $trackingCodeProperty->getValue($sentOtpDto));
+    }
+
+    public function test_toArray_returns_correct_array(): void
+    {
+        $code = 123456;
+        $waitingTime = 60;
+        $trackingCode = '550e8400-e29b-41d4-a716-446655440000';
+
+        $sentOtpDto = new SentOtpDto($code, $waitingTime, $trackingCode);
+
+        $expectedArray = [
+            'code' => $code,
+            'tracking_code' => $trackingCode,
+            'waiting_time' => $waitingTime,
+        ];
+
+        $this->assertSame($expectedArray, $sentOtpDto->toArray());
+    }
+}
